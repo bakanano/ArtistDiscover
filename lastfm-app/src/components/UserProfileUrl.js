50f271512ee996f13570a6d694b080db9e7130c7
bakanano <narutoz0rs@live.com>
2023-05-04 19:53:25 +1200

    Rename filename

--- lastfm-app/src/components/UserProfileUrl.js
+++ lastfm-app/src/components/UserProfileUrl.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "../styles.css";
 
-export default function userProfileUrl({username}) {
+export default function UserProfileUrl({username}) {
   return (
     <div className="profileURL">http://www.last.fm/user/{username}</div>
   )
