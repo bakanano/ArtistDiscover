3c4f28ce08c0b4e6791459b700088f08594d66ed
bakanano <narutoz0rs@live.com>
2023-05-25 15:07:24 +1200

    Changed text in user component

--- lastfm-app/src/components/UserCard.js
+++ lastfm-app/src/components/UserCard.js
@@ -1,4 +1,7 @@
-import React from "react"
+import React from "react";
+import DownloadIcon from '@mui/icons-material/Download';
+import GetAppIcon from '@mui/icons-material/GetApp';
+import IconButton from "@mui/material/IconButton";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -8,14 +11,15 @@ import MusicNoteIcon from "@mui/icons-material/MusicNote";
 import Tooltip from '@mui/material/Tooltip';
 import TodayIcon from '@mui/icons-material/Today';
 import moment from "moment";
+import html2canvas from "html2canvas";
 
 export default function UserCard(props) {
-  console.log(props)
-  const {name, country, image, playcount, registered, type} = props.userData.user;
+  const {name, country, image, playcount, registered, type, url} = props.userData.user;
   const avatarUrl = image?.[3]?.["#text"];
+  const profileUrl = url;
   const hasLocation = country !== "None";
   const registeredDate = moment.unix(registered.unixtime).format("DD MMM YYYY");
-  
+  const [imageDataUri, setImageDataUri] = React.useState(null);
   let typeColor = "";
 
   switch(type) {
@@ -25,7 +29,7 @@ export default function UserCard(props) {
     case "subscriber":
       typeColor = "subscriberColor";
       break;
-    case "alumn":
+    case "alum":
       typeColor = "alumColor";
       break;
     case "mod":
@@ -36,57 +40,73 @@ export default function UserCard(props) {
       break;
   }
 
+  function handleDownload() {
+  }
+
   return (
-    <section className="userCard">
+    <section className="cardContainer">
       <Card
-        style={{
-          width: 300,
-          height: 400
-        }}>
+        className="userCard"
+      >
       <CardHeader
-        avatar={<Avatar src={avatarUrl}/>}
+        avatar={<a href={profileUrl}><Avatar src={avatarUrl}/></a>}
         title={
           <>
-            {name} <span className={typeColor}>{type.toUpperCase()}</span>
-            </>
+            {name} <span className={typeColor}> {type.toUpperCase()}</span>
+          </>
         }
-        subheader={hasLocation && (
-          <Typography variant="body2" component="p">
-            {country}
-          </Typography>
-        )}
+        subheader={
+          hasLocation ? (
+            <div className="subheaderWithLocation">
+              <Typography variant="body2" component="p">
+                {country} <Tooltip title={`${Number(playcount).toLocaleString()} scrobbles`}>
+                  <MusicNoteIcon style={{ fontSize: 25 }} />
+                </Tooltip>
+                <Tooltip title={`Scrobbling since ${registeredDate}`}>
+                  <TodayIcon style={{ fontSize: 25 }} />
+                </Tooltip>
+              </Typography>
+            </div>
+          ) : (
+            <div className="subheaderWithoutLocation">
+              <div className="icons">
+                <Tooltip title={`${Number(playcount).toLocaleString()} scrobbles`}>
+                  <MusicNoteIcon style={{ fontSize: 25 }} />
+                </Tooltip>
+                <Tooltip title={`Scrobbling since ${registeredDate}`}>
+                  <TodayIcon style={{ fontSize: 25 }} />
+                </Tooltip>
+              </div>
+            </div>
+          )
+        }
+        // action={
+        //   <IconButton onClick={handleDownload}>
+        //     <DownloadIcon/>
+        //   </IconButton>
+          
+        // }
       />
       <CardContent>
-      <div className="icons">
-        <Tooltip title={`${Number(playcount).toLocaleString()} scrobbles`}>
-          <MusicNoteIcon
-            style={{
-            fontSize: 25
-            }}
-          />
-        </Tooltip>
-        <Tooltip title={`Scrobbling since ${registeredDate}`}>
-          <TodayIcon
-            style={{
-              fontSize: 25
-            }}
-          />
-        </Tooltip>
-      </div>
-      {props.topArtist && (
+          {props.recommendation && (
             <div>
-              <Typography variant="h6" component="h6" style={{ marginTop: "16px" }}>
-                Top 3 Artists for {name}:
-              </Typography>
-              <ul>
-                {props.topArtist.artist.map((artist) => (
-                  <li key={artist.name}>{artist.name} - {Number(artist.playcount).toLocaleString()} scrobbles</li>
-                ))}
-              </ul>
+              <Typography variant="h6" component="h6" align="center"><b>
+            
+              </b></Typography>
+                <ul>
+                  {props.recommendation.map((artist) => (
+                    <li key={artist}>{artist}</li>
+                  ))}
+                </ul>
             </div>
-      )}
+          )}
       </CardContent>
     </Card>
+
+    {imageDataUri && (
+      <a href={imageDataUri} download="user-card.png">
+      </a>
+    )}
     </section>
   )
 }
